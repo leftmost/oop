5b68b9326918c72b9da0b3e8fa633bc7e53b3a04
lorenzo <lorenzo@air-di-giulia-4>
2018-02-04 11:12:18 +0100

    Query registrazione

--- src/Model/DAO/Concrete/UtenteDAO.java
+++ src/Model/DAO/Concrete/UtenteDAO.java
@@ -12,81 +12,137 @@ import Model.DAO.Interface.UtenteDAOint;
 import Model.Database.Database;
 
 public class UtenteDAO implements UtenteDAOint {
-	
+
 	private static final String
 	INSERIMENTO = "INSERT INTO Utente(username, email, password,nome, cognome, tipologia) VALUES (?, ?, ?, ?, ?,?);";
-	
+
 	private static final String
 	RICERCA = "SELECT * FROM utente WHERE username = ?;";
-	
+
 	private static final String
 	AGGIORNAMENTO = "UPDATE utente SET tipologia=? ;";
-	
-	
+
+	private static final String
+	EMAIL_ESISTENTE = "SELECT * FROM utente WHERE email = ?;";
+
+	private static final String
+	USERNAME_ESISTENTE = "SELECT * FROM utente WHERE username = ?;";
+
+
 	//inserisce un nuovo User
 	@Override
 	public int inserisciUser(Utente utente) throws SQLException {
-		 Connection connection = Database.openConnection();
-		    PreparedStatement ps = connection.prepareStatement(INSERIMENTO);
-		    ps.setString(1, utente.getUsername());
-		    ps.setString(2, utente.getEmail());
-		    ps.setString(3, utente.getPassword());
-		    ps.setString(4, utente.getNome());
-		    ps.setString(5, utente.getNome());
-		    ps.setString(6, utente.getTipologia());
-		  
-		    int result = ps.executeUpdate();
-		    
-		    ps.close();
-		    connection.close();
-		    return result;
+		Connection connection = Database.openConnection();
+		PreparedStatement ps = connection.prepareStatement(INSERIMENTO);
+		ps.setString(1, utente.getUsername());
+		ps.setString(2, utente.getEmail());
+		ps.setString(3, utente.getPassword());
+		ps.setString(4, utente.getNome());
+		ps.setString(5, utente.getNome());
+		ps.setString(6, utente.getTipologia());
+
+		int result = ps.executeUpdate();
+
+		ps.close();
+		connection.close();
+		return result;
 	}
 
 	//ricerca un User 
 	@Override
 	public Utente ricercaUser(String username) throws SQLException {
-		
+
 		Utente utente=null;
 		Connection connection = Database.openConnection();  
 		PreparedStatement ps = connection.prepareStatement(RICERCA);
 		ps.setString(1, username);
 
 		ResultSet result = ps.executeQuery();
-		if ( result.first() == false ) {
-			return null;
-		}
 
-		utente = new Utente(result.getString(1), result.getString(2), result.getString(3), result.getString(4), result.getString(5), result.getString(6));
-		TimelineDAOint timeline = new TimelineDAO();
-		Timeline usersTimeline = timeline.ricercaTimeline(utente.getUsername());
-		utente.setTimeline(usersTimeline);
+		if ( result.first() == false ) {
+			utente=null;
+		}else {
+
+			utente = new Utente(result.getString(1), result.getString(2), result.getString(3), result.getString(4), result.getString(5), result.getString(6));
+
+			TimelineDAOint timeline = new TimelineDAO();
+
+			Timeline usersTimeline = timeline.ricercaTimeline(utente.getUsername());
+
+			utente.setTimeline(usersTimeline);
+
+		}
 		ps.close();
 		result.close();
 		connection.close();
 		return utente;
 	}
-	
-	
+
+
 	//promuove o retrocede un User
 	@Override
 	public int aggiornaUser(Utente utente) throws SQLException {
-		 Connection connection = Database.openConnection();
-		    PreparedStatement ps = connection.prepareStatement(AGGIORNAMENTO);
-		    ps.setString(1, utente.getUsername());
-		    ps.setString(2, utente.getEmail());
-		    ps.setString(3, utente.getPassword());
-		    ps.setString(4, utente.getNome());
-		    ps.setString(5, utente.getNome());
-		    ps.setString(6, utente.getTipologia());
-		  
-		    int result = ps.executeUpdate();
-		    
-		    ps.close();
-		    connection.close();
-		    return result;
+		Connection connection = Database.openConnection();
+		PreparedStatement ps = connection.prepareStatement(AGGIORNAMENTO);
+		ps.setString(1, utente.getUsername());
+		ps.setString(2, utente.getEmail());
+		ps.setString(3, utente.getPassword());
+		ps.setString(4, utente.getNome());
+		ps.setString(5, utente.getNome());
+		ps.setString(6, utente.getTipologia());
+
+		int result = ps.executeUpdate();
+
+		ps.close();
+		connection.close();
+		return result;
 	}
-	
-	
-	
-	
+
+	@Override
+	public boolean emailEsistente(String email) throws SQLException {
+		boolean emailEsistente;
+
+		Connection connection = Database.openConnection();  
+		PreparedStatement ps = connection.prepareStatement(EMAIL_ESISTENTE);
+		ps.setString(1, email);
+
+		ResultSet result = ps.executeQuery();
+		if ( result.first() == false ) {
+			emailEsistente=false;
+		}else {
+			emailEsistente=true;
+		}
+
+		ps.close();
+		result.close();
+		connection.close();
+		return emailEsistente;
+	}
+
+	@Override
+	public boolean usernameEsistente(String username) throws SQLException {
+		boolean usernameEsistente;
+
+		Connection connection = Database.openConnection();  
+		PreparedStatement ps = connection.prepareStatement(USERNAME_ESISTENTE);
+		ps.setString(1, username);
+
+		ResultSet result = ps.executeQuery();
+		if ( result.first() == false ) {
+			usernameEsistente=false;
+		}else {
+			usernameEsistente=true;
+		}
+
+		ps.close();
+		result.close();
+		connection.close();
+		return usernameEsistente;
+	}
+
+
+
+
+
+
 }
--- src/Model/DAO/Interface/UtenteDAOint.java
+++ src/Model/DAO/Interface/UtenteDAOint.java
@@ -2,6 +2,7 @@ package Model.DAO.Interface;
 import java.sql.SQLException;
 
 import Model.*;
+import Model.DAO.Concrete.UtenteDAO;
 
 public interface UtenteDAOint {
 	
@@ -11,4 +12,20 @@ public interface UtenteDAOint {
 	
 	public int aggiornaUser(Utente utente) throws SQLException;
 	
+	public boolean emailEsistente(String email) throws SQLException;
+
+	public boolean usernameEsistente(String username) throws SQLException;
+	
+	//TEST
+	public static void main(String[] args){
+		UtenteDAOint i = new UtenteDAO();
+		
+		try {
+			System.out.println(i.emailEsistente("andreoli@univaq.it"));
+			System.out.println(i.usernameEsistente("loreadnD"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
